f347708f58d1f1e6d00a34d722f0466c93a36fa6
Глеб Коньшин <130678774+yamarks@users.noreply.github.com>
2024-03-08 18:48:12 +0300

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+// Пример функции для копирования строки
+std::string CopyString(const char* str);
+
+// Другие вспомогательные функции могут быть добавлены по аналогии
+
+#endif // UTILS_H
